dafb373a2da1ec7b6a67de04c4701986dc8eaa36
Juan Daniel Luna Cienfuegos <milfuegosdev@gmail.com>
2023-10-18 10:40:12 -0600

    minor changes

--- app/controllers/passport-config.js
+++ app/controllers/passport-config.js
@@ -11,18 +11,20 @@ function initialize(passport) {
       async (username, password, done) => {
         try {
           const foundUser = await userModel.findOne(username);
-          if (!foundUser) {
+          // Si no encuentra algún usuario
+          if (!(foundUser.length > 0)) {
             return done(null, false, "El usuario no está registrado.");
           }
-
+          // Compara el hash de la contraseña con la contraseña ingresada.
           const passwordMatch = await bcrypt.compare(
             password,
             foundUser[0].password
           );
           if (passwordMatch) {
-            return done(null, foundUser[0]);
+            // Si las contraseñas coinciden
+            return done(null, foundUser[0]); // devuelve el usuario 
           } else {
-            return done(null, false, "La contraseña es incorrecta.");
+            return done(null, false, "La contraseña es incorrecta."); // Sino indica que no es la contraseña
           }
         } catch (err) {
           console.error(err);
