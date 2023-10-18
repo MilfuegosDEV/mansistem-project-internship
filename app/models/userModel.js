ad7581e3599e422a65c89dd862d187635e84103c
Juan Daniel Luna Cienfuegos <milfuegosdev@gmail.com>
2023-10-18 14:55:18 -0600

    getAll method

--- app/models/userModel.js
+++ app/models/userModel.js
@@ -6,9 +6,16 @@ db.query = util.promisify(db.query);
 
 class User {
   /**
-   * Busca un usuario por medio de su username
-   * @returns {Promise<Array>} Una promesa que se resuelve con un arreglo de la información del usuario.
+   * obtiene toda la información de los usuarios
+   * @returns {Promise<Array>} Una promesa que se resuelve con un arreglo de la información de los usuario.
    */
+  static async getAll() {
+    const query =
+      "SELECT user.id, user.name, user.lastname, user.username, user.email, user.password,uR.name AS roleName, province.name AS provinceName FROM users AS user INNER JOIN userRoles AS uR ON user.role_id = uR.id INNER JOIN provinces AS province ON user.province_id = province.id;";
+    const usersInfo = await db.query(query);
+    return usersInfo;
+  }
+
   static async findOne(username) {
     try {
       const query = "SELECT * FROM users WHERE username = ?";
