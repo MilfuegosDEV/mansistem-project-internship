4362532d24b743f1bec87486f8e1a642bc581e6a
MilfuegosDEV <MilfuegosDEV@gmail.com>
2023-11-16 15:19:11 -0600

    UserController including UserAudit

--- app/controllers/UserController.mjs
+++ app/controllers/UserController.mjs
@@ -1,21 +1,24 @@
 import db from "../../db/index.mjs";
 import bcryptjs from "bcryptjs";
-import Triggers from "./triggers/index.mjs";
+import Triggers from "../../db/triggers/index.mjs";
 
-export default class extends Triggers {
+class UserController extends Triggers {
+  constructor() {
+    super("USER", "USER_AUDIT");
+  }
   /**
-   * Añade un nuevo usuario
-   * @param {string} name Nombre del usuario
-   * @param {string} last_name Apellido del usuario
-   * @param {string} username Nombre de usuario
-   * @param {string} email Correo del usuario
-   * @param {string} password Contraseña del usuario
-   * @param {string} role Rol del usuario
-   * @param {string} province Provincia del usuario
-   * @param {string} performed_by_user_id usuario que añadió el perfil
-   * @returns {int} 1 si completa la acción de lo contrario 0;
+   * Inserta un nuevo usuario
+   * @param {string} name 
+   * @param {string} last_name 
+   * @param {string} username 
+   * @param {string} email 
+   * @param {string} password 
+   * @param {number} role 
+   * @param {number} province 
+   * @param {number} performed_by_user_id 
+   * @returns 
    */
-  static async add(
+  async add(
     name,
     last_name,
     username,
@@ -36,26 +39,25 @@ export default class extends Triggers {
       !performed_by_user_id
     )
       return 0;
-    try {
-      // Hash de la contraseña antes de almacenarla
-      const hashedPassword = await bcryptjs.hash(password, 10); // El '10' es el número de rondas de salting, considera configurarlo externamente
 
-      const query = `
+      try {
+        const hashedPassword = await bcryptjs.hash(password, 10);
+        const query = `
         INSERT INTO USER 
-          (name, last_name, username, email, password, role_id, province_id) 
+        (name, last_name, username, email, password, role_id, province_id) 
         VALUES 
-          (?, ?, ?, ?, ?, ?, ?)`;
-      await db.query(query, [
-        name.toUpperCase().trim(),
-        last_name.toUpperCase().trim(),
-        username.toLowerCase().trim(),
-        email.toLowerCase().trim(),
-        hashedPassword,
-        role,
-        province,
-      ]);
-
-      await this.auditUserCreation(performed_by_user_id);
+        (?, ?, ?, ?, ?, ?, ?)`;
+        await db.query(query, [
+          name.toUpperCase().trim(),
+          last_name.toUpperCase().trim(),
+          username.toLowerCase().trim(),
+          email.toLowerCase().trim(),
+          hashedPassword,
+          role,
+          province,
+        ]);
+        
+        await this.InsertionAudit(performed_by_user_id);
       return 1;
     } catch (error) {
       console.error("Error", error);
@@ -63,18 +65,18 @@ export default class extends Triggers {
     }
   }
   /**
-   * Actualiza la información del usuario
-   * @param {int} user_updated_id id del usuario
-   * @param {string} email Email del usuario
-   * @param {string} password Contraseña del usuario
-   * @param {int} role Rol del usuario
-   * @param {int} province Provincia del usuario
-   * @param {int} status Estado del usuario `{1: "habilitado", 0: 'inhabilitado'}`
-   * @param {int} performed_by_user_id id del usuario que realizó la acción
-   * @returns {int} 1 si completa la acción de lo contrario 0;
+   * Actualiza la información de un usuario
+   * @param {number} updated_id El usuario que será actualizado
+   * @param {string} email 
+   * @param {string} password 
+   * @param {number} role 
+   * @param {number} province 
+   * @param {number} status 
+   * @param {number} performed_by_user_id El usuario que realizó la actualización
+   * @returns 
    */
-  static async edit(
-    user_updated_id,
+  async edit(
+    updated_id,
     email,
     password,
     role,
@@ -83,7 +85,7 @@ export default class extends Triggers {
     performed_by_user_id
   ) {
     if (
-      !user_updated_id ||
+      !updated_id ||
       !email ||
       !password ||
       !role ||
@@ -92,31 +94,36 @@ export default class extends Triggers {
       !performed_by_user_id
     )
       return 0;
+
     try {
       const hashedPassword = await bcryptjs.hash(password, 10);
-      this.auditUserUpdate(performed_by_user_id, user_updated_id, {
+      await this.UpdateAudit(performed_by_user_id, updated_id, {
         email: email,
         password: hashedPassword,
         role_id: parseInt(role),
         province_id: parseInt(province),
         status_id: parseInt(status),
       });
+
       const query = `
         UPDATE USER 
           SET email=?,password=?, role_id=?, province_id = ?, status_id=?
         WHERE id=?;`;
-      await db.query(query, [
+      const result = await db.query(query, [
         email.toLowerCase().trim(),
         hashedPassword,
         role,
         province,
         status,
-        user_updated_id,
+        updated_id,
       ]);
-      return 1;
+
+      return result;
     } catch (error) {
       console.error("Error", error);
       return 0;
     }
   }
 }
+
+export default new UserController();
---
+++ db/triggers/index.mjs
@@ -0,0 +1,81 @@
+import db from "../index.mjs";
+
+export default class Triggers {
+  /**
+   * HISTORIAL DE CAMBIOS. ADEMÁS, INDICA QUIÉN REALIZÓ EL CAMBIO.
+   * @param {string} TABLE La tabla que será auditada.
+   * @param {string} TABLE_AUDIT La tabla que guarda el historial de cambios.
+   */
+  constructor(TABLE, TABLE_AUDIT) {
+    if (!TABLE) throw new Error("No se ha indicado la tabla a auditar.");
+    if (!TABLE_AUDIT)
+      throw new Error(
+        "No se ha indicado la tabla para guardar el historial de cambios."
+      );
+    this.TABLE = TABLE;
+    this.TABLE_AUDIT = TABLE_AUDIT;
+  }
+
+  /**
+   * Registra un nuevo registro dentro de la base de datos.
+   * @param {int} performed_by_user_id El USUARIO QUE REALIZÓ EL REGISTRO.
+   */
+  async InsertionAudit(performed_by_user_id) {
+    try {
+      const [last] = await db.query(
+        `SELECT id FROM ${this.TABLE} ORDER BY id DESC LIMIT 1;`
+      );
+
+      await db.query(
+        `INSERT INTO ${this.TABLE_AUDIT} (updated_id, action_performed, performed_by_user_id, field_changed) VALUES (?, ?, ?, ?);`,
+        [last[0].id, "INSERT", performed_by_user_id, `NEW ${this.TABLE}`]
+      );
+    } catch (err) {
+      console.error(err);
+      throw err;
+    }
+  }
+  /**
+   * Registra los cambios que se han realizado en la tabla usuarios. Además, indica quién los realizó.
+   * @param {int} performed_by_user_id El usuario que realizo el nombre
+   * @param {int} updated_id El registro que fue editado
+   * @param {object} editedFields // Los valores que se esperan que sean editados.
+   */
+  async UpdateAudit(performed_by_user_id, updated_id, editedFields) {
+    if (!performed_by_user_id || !updated_id || !editedFields)
+      throw new Error("Faltan parametros.");
+    const fields = Object.keys(editedFields);
+    try {
+      const [results] = await db.query(
+        `SELECT 
+            ${fields.join(",")}
+        FROM 
+            ${this.TABLE}
+        WHERE id=?`,
+        [updated_id]
+      );
+      const oldValues = results[0];
+      // Comparar los valores antiguos con los nuevos y auditar los cambios
+      fields.forEach(async (field) => {
+        const oldValue = oldValues[field];
+        const newValue = editedFields[field];
+
+        if (oldValue !== newValue) {
+          await db.query(
+            `INSERT INTO ${this.TABLE_AUDIT} (updated_id, action_performed, performed_by_user_id, field_changed, old_value, new_value) VALUES (?, ?, ?, ?, ?, ?);`,
+            [
+              updated_id,
+              "UPDATE",
+              performed_by_user_id,
+              field,
+              oldValue,
+              newValue,
+            ]
+          );
+        }
+      });
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+}
